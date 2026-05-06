c9a8098aaa369e0db41b4c048d2ae0789f7620f0
marcu <marcu@MCWINXMARC>
2026-05-06 17:37:31 +0100

    a new changed happening here

--- CodingGO.cpp
+++ CodingGO.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -9,8 +8,8 @@ int code1()
 	int choice;
 
 	cout << "What do you want to do?" << endl;
-	cout << "1. Detect genre from BPM" << endl;
-	cout << "2. Calculate beat duration" << endl;
+	cout << "1. Detect genre from BPM" << endl; // here they can input BPM and get the genre of music that is typically associated
+	cout << "2. Calculate beat duration" << endl; // here they can input BPM and get the duration of each beat in seconds
 	cout << "Choose: ";
 	cin >> choice;
 
